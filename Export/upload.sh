bc8a9b035d3073d2195755508002392b5b10e1f7
Norodix <peceli.bence@gmail.com>
2023-07-09 18:15:05 +0200

    Create exports

--- Export/upload.sh
+++ Export/upload.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-NAME="name"
-LINK="link"
+NAME="ShoutAtTheCat"
+LINK="shout-at-the-cat"
 
 cd "${0%/*}" #must change to own directory for bash
-cp "./${NAME}_HTML5/${NAME}.html" "./${NAME}_HTML5/index.html"
+#cp "./${NAME}_HTML5/${NAME}.html" "./${NAME}_HTML5/index.html"
 make
-butler push "${NAME}_HTML5"        "Norodix/$LINK:html"
+#butler push "${NAME}_HTML5"        "Norodix/$LINK:html"
 butler push "${NAME}_Windows.zip"  "Norodix/$LINK:windows"
 butler push "${NAME}_Linux.zip"    "Norodix/$LINK:linux"
